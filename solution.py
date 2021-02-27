3a00b22a345f08df30dec8750a428fc3bb2d92e5
paradagan <78583154+paradagan@users.noreply.github.com>
2021-02-26 22:15:57 -0700

    remove unnecessary code

--- solution.py
+++ solution.py
@@ -31,25 +31,26 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     mail_from_command = 'MAIL FROM:<pg1960@nyu.com>'
     clientSocket.send(mail_from_command.encode())
-    recv2 = clientSocket.recv(1024).decode()
+
     # Fill in end
 
     # Send RCPT TO command and print server response.
     # Fill in start
     rcpt_to_command = 'RCPT TO:<alice@example.com>'
     clientSocket.send(rcpt_to_command.encode())
-    recv3 = clientSocket.recv(1024).decode()
+    
     # Fill in end
 
     # Send DATA command and print server response.
     # Fill in start
     data_command = 'DATA'
     clientSocket.send(data_command.encode())
-    recv4 = clientSocket.recv(1024).decode()
+
     # Fill in end
 
     # Send message data.
     # Fill in start
+
     # Fill in end
 
     # Message ends with a single period.
